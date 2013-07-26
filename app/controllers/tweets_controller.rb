6354a12d2db7410868f4214c24afa9ab4708b4f7
andrea delrio <delrio.andre@gmail.com>
2013-07-26 01:07:13 -0400

    First basic version

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,38 @@
+class TweetsController < ApplicationController
+  before_action :check_user_login
+
+  def index
+    @tweets = Tweet.all
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def destroy
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user = User.first
+    @tweet.save
+    redirect_to user_path(@tweet.user)
+  end
+
+  
+  private
+
+  def check_user_login
+    params.require(:tweet).permit(:content)
+  end
+
+  def tweet_params
+    params.require(:tweet).permit(:content)
+  end
+
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Twitter::Application.config.secret_key_base = '7c80b647a78ce005a7e7597f8e13af4c21a2f54301826e55aae76eff158e40b78d98a2e814fe68e49fbf7fa76fef2f130fe2f6b77f0f4d14737f4560cacc0f41'
